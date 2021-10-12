bca16aeea136d19df239649a6ef9f0b8aa429ff7
LudwinLara21 <87671764+LudwinLara21@users.noreply.github.com>
2021-10-12 14:46:48 -0600

    Nuevo

---
+++ OperacionesBasicas/OperacionesBasicas/Negocios/ClsOperaciones.cs
@@ -0,0 +1,35 @@
+﻿using OperacionesBasicas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionesBasicas.Negocios
+{
+    class ClsOperaciones
+    {
+        public double Suma(Operaciones operacion)
+        {
+            operacion.Resultado = operacion.Dato1 + operacion.Dato2;
+            return operacion.Resultado;
+        }
+        public double Resta(Operaciones operacion)
+        {
+            operacion.Resultado = operacion.Dato1 - operacion.Dato2;
+            return operacion.Resultado;
+        }
+
+        public double Multiplicacion(Operaciones operacion)
+        {
+            operacion.Resultado = operacion.Dato1 * operacion.Dato2;
+            return operacion.Resultado;
+        }
+
+        public double Division(Operaciones operacion)
+        {
+            operacion.Resultado = operacion.Dato1 / operacion.Dato2;
+            return operacion.Resultado;
+        }
+    }
+}
